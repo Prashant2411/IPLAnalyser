e33c6d16be5665792954069600e8337fadfdb902
Prashant2411 <prashantbedi241197@gmail.com>
2020-01-18 11:39:21 +0530

    Added: Test case and code to sort according to averages

--- src/main/java/IPLAnalyser.java
+++ src/main/java/IPLAnalyser.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.nio.file.Files.newBufferedReader;
 
@@ -24,5 +26,17 @@ public class IPLAnalyser {
         } catch (CSVBuilderException e) {
             throw new IPLAnalyserException(e.getMessage(),e.type.name());
         }
+    }
+
+    public void getSorted(Comparator<MostRunsCSV> comp) {
+        csvList = csvList.stream()
+                .sorted(comp)
+                .collect(Collectors.toList());
+    }
+
+    public List getSortedBattingAverages() {
+        Comparator<MostRunsCSV> comp = (obj1, obj2) -> (obj1.avg - obj2.avg) > 0 ? -1 : 1;
+        getSorted(comp);
+        return csvList;
     }
 }
--- src/test/java/IPLAnalyserTest.java
+++ src/test/java/IPLAnalyserTest.java
@@ -1,17 +1,33 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class IPLAnalyserTest {
-    public String MOST_RUNS_CSV = "/home/admin1/IdeaProjects/IPL_Analyser/src/test/resources/Batting.csv";
+    public String MOST_RUNS_CSV = "/home/admin1/IdeaProjects/IPLAnalyser/src/test/resources/Batting.csv";
 
     @Test
     public void givenMostRunCSV_shouldReturnCorrectValues() {
         try {
             IPLAnalyser iplAnalyser = new IPLAnalyser();
             int size = iplAnalyser.loadBattingAverages(MOST_RUNS_CSV);
-            Assert.assertEquals(100,size);
+            Assert.assertEquals(100, size);
         } catch (IPLAnalyserException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void givenIPLCSV_shouldReturnTopBattingAverages() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            int size = iplAnalyser.loadBattingAverages(MOST_RUNS_CSV);
+            List<MostRunsCSV> sortedList = iplAnalyser.getSortedBattingAverages();
+            Assert.assertEquals(83.2, sortedList.get(0).avg, 0);
+            Assert.assertEquals(0.0, sortedList.get(99).avg, 0);
+        } catch (IPLAnalyserException e) {
+            e.printStackTrace();
+        }
+
     }
 }
